1583e1cea3df5154cba7c926d48ad61bfab5c42b
LucianoDominguez768 <lucianodominguezarellano2233@gmail.com>
2025-10-11 12:28:08 -0300

    trabajo practico 2 punto 13.py

--- trabajo practico 2 punto 13.py
+++ trabajo practico 2 punto 13.py
@@ -1,3 +1,18 @@
+"""13. Dada una pila con los trajes de Iron Man utilizados en las películas de Marvel Cinematic Uni-
+verse (MCU) de los cuales se conoce el nombre del modelo, nombre de la película en la que se
+
+usó y el estado en que quedó al final de la película (Dañado, Impecable, Destruido), resolver
+las siguientes actividades:
+a. determinar si el modelo Mark XLIV (Hulkbuster) fue utilizado en alguna de las películas,
+además mostrar el nombre de dichas películas;
+b. mostrar los modelos que quedaron dañados, sin perder información de la pila.
+c. eliminar los modelos de los trajes destruidos mostrando su nombre;
+d. un modelo de traje puede usarse en más de una película y en una película se pueden usar
+más de un modelo de traje, estos deben cargarse por separado;
+e. agregar el modelo Mark LXXXV a la pila, tener en cuenta que no se pueden cargar modelos
+repetidos en una misma película;
+f. mostrar los nombre de los trajes utilizados en las películas “Spider-Man: Homecoming” y
+“Capitan America: Civil War”."""
 
 from stack import Stack
 
@@ -100,5 +115,6 @@ print()
 agregar_mark_lxxxv(pila_trajes, "Avengers: Endgame")
 print()
 mostrar_trajes_peliculas(pila_trajes, ["Spider-Man: Homecoming", "Capitan America: Civil War"])
+
 
 
