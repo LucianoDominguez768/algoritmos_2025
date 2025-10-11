b70e453bf22ba096b9288bcd01e7284d0ebc1eb9
LucianoDominguez768 <lucianodominguezarellano2233@gmail.com>
2025-10-11 12:37:09 -0300

    trabajo practico 1 recursividad.py

--- trabajo practico 1/trabajo practico 1 recursividad.py
+++ trabajo practico 1/trabajo practico 1 recursividad.py
@@ -1,4 +1,4 @@
-#punto 5 
+#5. Desarrollar una función que permita convertir un número romano en un número decimal.
 
 def romano_a_decimal_rec(romano):
     valores = {
@@ -33,7 +33,17 @@ def romano_a_decimal_rec(romano):
 
 print(romano_a_decimal_rec("MCMXCIV"))
 
-#punto 22
+"""El problema de la mochila Jedi. Suponga que un Jedi (Luke Skywalker, Obi-Wan Kenobi, Rey u
+otro, el que más le guste) está atrapado, pero muy cerca está su mochila que contiene muchos
+objetos. Implementar una función recursiva llamada “usar la fuerza” que le permita al Jedi “con
+ayuda de la fuerza” realizar las siguientes actividades:
+a. sacar los objetos de la mochila de a uno a la vez hasta encontrar un sable de luz o que no
+queden más objetos en la mochila;
+
+b. determinar si la mochila contiene un sable de luz y cuantos objetos fueron necesarios sa-
+car para encontrarlo;
+
+c. Utilizar un vector para representar la mochila."""
 
 def usar_la_fuerza(mochila, objetos_sacados=0):
    
@@ -52,3 +62,4 @@ def usar_la_fuerza(mochila, objetos_sacados=0):
     else:
        
         return usar_la_fuerza(mochila, objetos_sacados)
+
